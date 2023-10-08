1aeb9aa6c86ab3b3559add342fda06c04419d48a
GAELSUSTAITA64 <2030048@upv.edu.mx>
2023-10-08 04:30:40 -0600

    login y validacion grupos1

---
+++ login.php
@@ -0,0 +1,59 @@
+<?php
+// Conexión a la base de datos
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "rfid_db";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+// Obtener datos del formulario
+$matricula = $_POST['matricula'];
+$nip = $_POST['nip'];
+
+// Verificar el primer caracter de la matricula
+$primerCaracter = substr($matricula, 0, 1);
+
+if ($primerCaracter === 'a') {
+    // Consultar la tabla de alumnos
+    $sql = "SELECT * FROM alumnos WHERE matricula = '$matricula' AND nip = '$nip'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // Autenticación exitosa para alumno
+        header("Location: pages/tables/data.html");
+        exit();
+    } else {
+        // Autenticación fallida, redirigir al formulario de inicio de sesión
+        header("Location: index.html");
+        exit();
+    }
+} elseif ($primerCaracter === 'm') {
+    // Consultar la tabla de maestros
+    $sql = "SELECT * FROM maestros WHERE matricula = '$matricula' AND nip = '$nip'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // Autenticación exitosa para maestro
+        $maestro = $result->fetch_assoc();
+        $idUsuario = $maestro['id_maestro'];
+        // Redirigir a la página de maestro con el ID del usuario en la URL
+        header("Location: pages/tables/simple2.html?id=" . $idUsuario);
+        exit();
+    } else {
+        // Autenticación fallida, redirigir al formulario de inicio de sesión
+        header("Location: index.html");
+        exit();
+    }
+} else {
+    // Carácter desconocido en la matrícula, redirigir al formulario de inicio de sesión
+    header("Location: index.html");
+    exit();
+}
+
+$conn->close();
+?>
--- pages/tables/get_grupos.php
+++ pages/tables/get_grupos.php
@@ -11,8 +11,15 @@ if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
-// Consulta SQL para obtener todos los grupos
-$sql = "SELECT grupo_id, nombre_grupo FROM grupos";
+// Obtener el ID del maestro (supongamos que lo pasas como parámetro)
+$idMaestro = $_GET['id_maestro']; // Cambia esto según cómo obtienes el ID del maestro
+
+// Consulta SQL para obtener los grupos que el maestro dirige
+$sql = "SELECT grupos.grupo_id, grupos.nombre_grupo
+        FROM grupos
+        INNER JOIN maestros_grupos ON grupos.grupo_id = maestros_grupos.grupo_id
+        WHERE maestros_grupos.id_maestro = $idMaestro";
+
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -22,7 +29,7 @@ if ($result->num_rows > 0) {
     }
     echo json_encode($grupos);
 } else {
-    echo "No se encontraron grupos.";
+    echo "El maestro no dirige ningún grupo.";
 }
 
 $conn->close();
